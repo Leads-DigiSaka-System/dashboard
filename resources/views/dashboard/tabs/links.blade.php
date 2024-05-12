22eaaa8c8ee8187deb418bada8a58b0280efcf14
Jayson Gamilla <jayson.gamilla@recruitonline.com.au>
2024-05-13 06:22:34 +0800

    new link, login response type

--- resources/views/dashboard/tabs/links.blade.php
+++ resources/views/dashboard/tabs/links.blade.php
@@ -11,6 +11,13 @@
         <div class="col-md-12 col-lg-6">
             <div class="card rounded-3 shadow-sm">
                 <div class="card-body">
+                    <iframe class="w-100" style="height:800px;" src="https://ee-brybaltazar.projects.earthengine.app/view/philippine-sugarcane-area" title="philippine sugarcane area app"></iframe>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-12 col-lg-6">
+            <div class="card rounded-3 shadow-sm">
+                <div class="card-body">
                     <a class="twitter-timeline" data-lang="en" data-height="800" href="https://twitter.com/dost_pagasa?ref_src=twsrc%5Etfw">Tweets by dost_pagasa</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> 
                 </div>
             </div>
