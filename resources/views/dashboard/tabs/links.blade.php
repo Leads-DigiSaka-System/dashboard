7ef873a92e7b2997ae470bd270fb91a2d6ba3562
Jayson Gamilla <jayson.gamilla@recruitonline.com.au>
2024-05-15 10:46:38 +0800

    Links for PH Maize Area

--- resources/views/dashboard/tabs/links.blade.php
+++ resources/views/dashboard/tabs/links.blade.php
@@ -19,6 +19,16 @@
                 </div>
             </div>
         </div>
+        <div class="col-md-12 col-lg-6">
+            <div class="card rounded-3 shadow-sm">
+                <div class="card-body">
+                    <a href="https://ee-brybaltazar.projects.earthengine.app/view/philippine-maize" target="_blank">
+                        <img class="w-100" style="height:800px;" src="{{ asset('images/Maize_Area.png') }}" alt="Sugar Cane">
+                    </a>
+                </div>
+            </div>
+        </div>
+        
         
         <div class="col-md-12 col-lg-6">
             <div class="card rounded-3 shadow-sm">
