85e997c04587191154dac9151d86c620faab85bc
Jayson Gamilla <jayson.gamilla@recruitonline.com.au>
2024-05-29 15:11:05 +0800

    prism link

--- resources/views/dashboard/tabs/links.blade.php
+++ resources/views/dashboard/tabs/links.blade.php
@@ -28,7 +28,15 @@
                 </div>
             </div>
         </div>
-    
+        <div class="col-md-12 col-lg-6">
+            <div class="card rounded-3 shadow-sm">
+                <div class="card-body">
+                    <a href="https://prism.philrice.gov.ph/dataproducts/" target="_blank">
+                        <img class="w-100" style="height:800px;" src="{{ asset('images/Prism_area.png') }}" alt="Rice Area">
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
     
 </div>
