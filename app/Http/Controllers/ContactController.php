ad32b790e9e899eaff8f1d72eea842007c07c122
Jayson Gamilla <jayson.gamilla@recruitonline.com.au>
2024-07-19 07:35:19 +0800

    fix datatable function

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -24,52 +24,47 @@ class ContactController extends Controller
     }
 
     public function index(Request $request, User $user){
-        if($request->ajax()){
-            $restriction = $this->getRegionFilter();
-            $users = DB::table("contacts")->where(
-                function($query) use ($restriction) {
+            if($request->ajax()){
+                $restriction = $this->getRegionFilter();
+                $users = User::where(function($query) use ($restriction) {
                     $query->where('region', 'like', $restriction);
                     if($restriction == "%%"){
                         $query->orWhereNull('region');
                     }
-                }
-            )->get();
-            return datatables()
-                ->of($users)
-                ->addIndexColumn()
-                ->addColumn('status', function ($user) {
-                    return '<span class="badge badge-light-' . $user->getStatusBadge() . '">' . $user->getStatus() . '</span>';
-                })
-                ->addColumn('phone_number', function ($user) {
-                    return $user->phone_number ? $user->phone_number : 'N/A';
-                })
-                ->addColumn('role', function ($user) {
-                    return $user->role_title ? $user->role_title : 'N/A';
-                })
-                ->addColumn('via_app', function ($user) {
-                    return $user->via_app == 1 ? "YES" : 'NO';
-                })
-                ->addColumn('employee_id', function ($user) {
-                    return $user->employee_id ? $user->employee_id : 'N/A';
-                })
-                ->addColumn('registered_date', function ($user) {
-                    return $user->registered_date ? $user->registered_date : 'N/A';
-                })
-                ->addColumn('action', function ($user) {
-                    $btn = '';
-                    $btn .= '<button class="btn btn-primary" onclick="handleViewProfile(\''.encrypt($user->id).'\')">View Profile</button>&nbsp;&nbsp;';
-                    $btn .= '<button class="btn btn-primary" onclick="handleContactProfile(\''.encrypt($user->id).'\')">View Contact</button>';
-                    // $btn = '<a href="' . route('farmers.show', encrypt($user->id)) . '" title="View"></a>&nbsp;&nbsp;';
-                    // $btn .= '<a href="javascript:void(0);" delete_form="delete_customer_form"  data-id="' . encrypt($user->id) . '" class="delete-datatable-record text-danger delete-users-record" title="Delete"><i class="fas fa-trash"></i></a>';
-                    return $btn;
-                })
-                ->rawColumns([
-                    'action',
-                    'status',
-                ])
-            ->make(true);
+                })->get();
+        
+                return datatables()
+                    ->of($users)
+                    ->addIndexColumn()
+                    ->addColumn('status', function ($user) {
+                        return '<span class="badge badge-light-' . $user->getStatusBadge() . '">' . $user->getStatus() . '</span>';
+                    })
+                    ->addColumn('phone_number', function ($user) {
+                        return $user->phone_number ? $user->phone_number : 'N/A';
+                    })
+                    ->addColumn('role', function ($user) {
+                        return $user->role_title ? $user->role_title : 'N/A';
+                    })
+                    ->addColumn('via_app', function ($user) {
+                        return $user->via_app == 1 ? "YES" : 'NO';
+                    })
+                    ->addColumn('employee_id', function ($user) {
+                        return $user->employee_id ? $user->employee_id : 'N/A';
+                    })
+                    ->addColumn('registered_date', function ($user) {
+                        return $user->registered_date ? $user->registered_date : 'N/A';
+                    })
+                    ->addColumn('action', function ($user) {
+                        $btn = '';
+                        $btn .= '<button class="btn btn-primary" onclick="handleViewProfile(\''.encrypt($user->id).'\')">View Profile</button>&nbsp;&nbsp;';
+                        $btn .= '<button class="btn btn-primary" onclick="handleContactProfile(\''.encrypt($user->id).'\')">View Contact</button>';
+                        return $btn;
+                    })
+                    ->rawColumns(['action', 'status'])
+                    ->make(true);
+            }
+        
+            return view('contacts.index');
         }
-
-        return view('contacts.index');
-    }
+    
 }
