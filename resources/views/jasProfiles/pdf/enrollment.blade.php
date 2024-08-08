1cb072d5ec0b9d7086b26871ba36c221612e906c
esonmiles <gamillajayson.work@gmail.com>
2024-08-08 18:59:44 +0800

    JAS view Enrollment Form PDF template

--- resources/views/jasProfiles/pdf/enrollment.blade.php
+++ resources/views/jasProfiles/pdf/enrollment.blade.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Enrollment Form</title>
     <style>
-        @page { margin: 0px; }
-        body { margin: 0px; }
+        @page {
+            margin: 0px;
+        }
+
+        body {
+            margin: 0px;
+        }
+
         body {
             font-family: Arial, sans-serif;
             margin: 0;
@@ -14,45 +21,62 @@
             background-color: white;
             height: 100vh;
         }
+
         .container {
-            background-color: grey;
+            /* background-color: grey; */
             /* padding: 20px; */
             /* border-radius: 10px; */
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            /* box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); */
             /* width: 100%; */
             /* max-width: 800px; */
             margin: 0 50px;
             margin-right: 10px;
         }
-        .header img{
+
+        .header img {
             width: 100%;
             margin-bottom: 20px;
+        }
+
+        .footer {
+            position: fixed;
+            bottom: 0px;
+        }
+
+        .footer img {
+            width: 100%;
+            margin-top: 20px;
         }
 
         .header h1 {
             margin: 0;
             font-size: 24px;
         }
+
         .form-group {
             margin-bottom: 15px;
             width: 100%
         }
+
         .form-group label {
             display: inline-block;
             /* font-weight: bold; */
             /* margin-bottom: 5px; */
             font-size: 14px;
         }
+
         .form-group span {
             display: block;
             font-weight: normal;
             font-size: 12px;
             color: #666;
         }
+
         .form-group .underline {
             border-bottom: #000 solid black;
             display: inline-block;
         }
+
         .form-group input[type="text"],
         .form-group input[type="date"],
         .form-group input[type="tel"] {
@@ -62,45 +86,84 @@
             border: 1px solid #ccc;
             border-radius: 5px;
         }
+
         .form-group input[type="text"]:last-child,
         .form-group input[type="tel"]:last-child {
             margin-right: 0;
         }
+
         .form-group input[type="text"] {
             width: 100%;
         }
+
         .form-group .half {
             width: calc(50% - 10px);
         }
+
         .form-group .full {
             width: 100%;
         }
-        .signature-section {
-            text-align: center;
-            margin-top: 20px;
+
+        table {
+            width: 90%;
+            border-collapse: collapse;
+            margin: 0 auto;
         }
-        .signature-section p {
+
+        th,
+        td {
+            border: 1px solid #858585;
+            text-align: center;
+            padding: 8px;
+        }
+
+        th {
+            background-color: #f0ea3e;
+            color: black;
+        }
+
+        tbody.big-padding tr td {
+            padding-top: 35px;
+            padding-bottom: 35px;
+        }
+
+        tbody.small-padding tr td {
+            padding-top: 10px;
+            padding-bottom: 10px;
+        }
+
+        .no-lh {
+            margin-top: 2px;
+            line-height: 0px
+        }
+
+        .form-info tr th {
+            width: 30%;
             font-size: 14px;
+            font-weight: bold;
         }
-        .signature-section .signatures {
-            display: flex;
-            justify-content: space-between;
-            margin-top: 20px;
+
+        .form-info {
+            width: 85%;
         }
-        .signature-section .signatures div {
-            width: calc(33.333% - 10px);
-            text-align: center;
+
+        table.form-activity tbody > tr > td:first-child{
+            width: 25% !important;
+            text-transform: uppercase;
+            padding-top: 35px;
+            padding-bottom: 35px;
         }
-        .signature-section .signatures div p {
-            border-top: 1px solid #000;
-            margin: 0;
-            padding-top: 5px;
+
+        table.form-activity{
+            margin-top: 30px;
         }
     </style>
 </head>
+
 <body>
     <div class="header">
-        <img src="data:image/svg+xml;base64,{{ base64_encode(file_get_contents(public_path('images/efheader.jpg'))) }}" alt="Logo">
+        <img src="data:image/svg+xml;base64,{{ base64_encode(file_get_contents(public_path('images/efheader.jpg'))) }}"
+            alt="Logo">
     </div>
     <div class="container">
         <form>
@@ -127,41 +190,41 @@
                     <div class="underline" style="width: 220px"></div>
                     <span>(Numero ng Telepono, Kung Mayroon)</span>
                 </div>
-                
+
             </div>
             <div class="form-group" style="margin-top: 30px">
                 <div class="label-container" style="width: 30%; display: inline-block; padding-bottom: 55px">
                     <label for="variety-wet">VARIETY USUALLY USED</label>
-                    <span>(Binhing Ginagamit)</span>  
+                    <span>(Binhing Ginagamit)</span>
                 </div>
                 <div class="label-container" style="width: 68%; display: inline-block">
                     <label for="variety-wet">WET SEASON: </label>
                     <div class="underline" style="width: calc(100% - 130px);"></div>
-                    <span>(Tag Ulan)</span>  
+                    <span>(Tag Ulan)</span>
 
                     <label for="variety-dry" style="margin-top: 20px">DRY SEASON: </label>
                     <div class="underline" style="width: calc(100% - 130px);"></div>
                     <span>(Tag Tuyo)</span>
                 </div>
-                
+
             </div>
             <div class="form-group" style="margin-top: 20px">
                 <div class="label-container" style="width: 30%; display: inline-block; padding-bottom: 55px">
                     <label for="variety-wet">AVERAGE YIELD PER HECTARE</label>
-                    <span>(Dami ng Sako ng Ani)</span>  
+                    <span>(Dami ng Sako ng Ani)</span>
                 </div>
                 <div class="label-container" style="width: 68%; display: inline-block">
                     <label for="variety-wet">WET SEASON: </label>
                     <div class="underline" style="width: calc(100% - 130px);"></div>
-                    <span>(Tag Ulan)</span>  
+                    <span>(Tag Ulan)</span>
 
                     <label for="variety-dry" style="margin-top: 20px">DRY SEASON: </label>
                     <div class="underline" style="width: calc(100% - 130px);"></div>
                     <span>(Tag Tuyo)</span>
                 </div>
-                
+
             </div>
-           
+
             <div class="form-group">
                 <div class="label-container">
                     <label for="dealers">DEALER(S) WHERE YOU BUY FROM: </label>
@@ -170,25 +233,248 @@
                 <span>(Suking Tindahan)</span>
             </div>
             <div class="form-group" style="margin-top: 40px;text-align: center">
-                <label style="font-size: 16px">I hereby affix my signature to manifest my agreement to abide by the</label>
+                <label style="font-size: 16px">I hereby affix my signature to manifest my agreement to abide by
+                    the</label>
                 <span>Kalakip nito ang aking lagda bilang pagpapatunay na susunod ako sa alituntunin</span>
                 <label style="font-size: 16px">mechanics of this program.</label>
                 <span>ng programang ito.</span>
             </div>
-            <div class="signature-section">
-                <div class="signatures">
-                    <div>
-                        <p>TPS or CPR/SIGNATURE: _________________</p>
-                    </div>
-                    <div>
-                        <p>AM/SIGNATURE: _________________</p>
-                    </div>
-                    <div>
-                        <p>SIGNATURE (Lagda): _________________</p>
-                    </div>
+
+            <div class="form-group" style="margin-top: 30px; margin-left: 65%;width: 215px;text-align:center">
+                <div class="underline" style="width: 100%;margin-bottom: 0px;"></div><br>
+                <label for="name" style="margin-top: 0px; line-height: 0px">SIGNATURE</label>
+                <span style="margin-top: 0px; line-height: 0px">(Lagda)</span>
+            </div>
+            <div class="form-group">
+                <div class="label-container">
+                    <label for="dealers" style="font-size: 12px">TPS or CPR/SIGNATURE: </label>
+                    <div class="underline" style="width: 150px"></div>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="label-container">
+                    <label for="dealers" style="font-size: 12px">AM/SIGNATURE: </label>
+                    <div class="underline" style="width: 150px"></div>
                 </div>
             </div>
         </form>
     </div>
+    <div class="footer">
+        <img src="data:image/svg+xml;base64,{{ base64_encode(file_get_contents(public_path('images/effooter.jpg'))) }}"
+            alt="Logo">
+    </div>
+    <div style="page-break-before: always;">
+        <div class="header">
+            <img src="data:image/svg+xml;base64,{{ base64_encode(file_get_contents(public_path('images/efheader2.jpg'))) }}"
+                alt="Logo">
+        </div>
+        <div class="">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Activity</th>
+                        <th>Timing</th>
+                        <th>Remarks</th>
+                        <th>Observation</th>
+                        <th>Signature</th>
+                    </tr>
+                </thead>
+                <tbody class="big-padding">
+                    <tr>
+                        <td>TRANSPLANTING</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>IRRIGATION</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>1st TOP DRESS</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>DE-IRRIGATE</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>HARVESTING</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <div
+                style="width: 90%; margin: 0 auto; text-align: center;font-size: 12px;font-weight: bold;margin-top: 20px">
+                <p>Please record all pesticides used in the participating farm with corresponding dosage rate and timing
+                </p>
+            </div>
+
+            <table style="width: 85%">
+                <thead style="font-size: 12px;font-weight: bold;">
+                    <tr>
+                        <th>PRODUCT</th>
+                        <th>PEST/DESEASE</th>
+                        <th>RATE 16LI/WATER</th>
+                        <th>TIMING OF APPLICATION</th>
+                    </tr>
+                </thead>
+                <tbody class="small-padding">
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div style="position: absolute; right: 80px; bottom: 50px; z-index: 2">
+        <div class="form-group">
+            <div class="label-container" class="no-lh">
+                <label for="dealers" style="font-size: 12px" class="no-lh">TPS or CPR/SIGNATURE: </label>
+                <div class="underline no-lh" style="width: 153px"></div>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="label-container" class="no-lh">
+                <label for="dealers" style="font-size: 12px" class="no-lh">AM/SIGNATURE: </label>
+                <div class="underline no-lh" style="width: 200px"></div>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="label-container" class="no-lh">
+                <label for="dealers" style="font-size: 12px" class="no-lh">VP for Sales: </label>
+                <div class="underline no-lh" style="width: 225px"></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer">
+        <img src="data:image/svg+xml;base64,{{ base64_encode(file_get_contents(public_path('images/effooter2.jpg'))) }}"
+            alt="Logo">
+    </div>
+    <div style="page-break-before: always;">
+        <div class="header">
+            <img src="data:image/svg+xml;base64,{{ base64_encode(file_get_contents(public_path('images/efheader3.jpg'))) }}"
+                alt="Logo">
+        </div>
+        <table class="form-info">
+            <tr>
+                <th>Name of TPS:</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>Participant:</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>Area:</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>Location:</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>Date Duration:</th>
+                <td></td>
+            </tr>
+        </table>
+
+        <table class="form-activity">
+            <thead>
+                <tr>
+                    <th>Activity</th>
+                    <th>Timing</th>
+                    <th>Remarks</th>
+                    <th>Observation</th>
+                    <th>Signature</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Seed Bed Preparation Phase 1</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Seed Bed Preparation Phase 2</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Seed Soaking</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Seed Sowing</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Basal Fertilization</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
+    <div class="footer">
+        <img src="data:image/svg+xml;base64,{{ base64_encode(file_get_contents(public_path('images/effooter.jpg'))) }}"
+            alt="Logo">
+    </div>
 </body>
+
 </html>
