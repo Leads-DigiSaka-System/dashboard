c6617fe5d87a6242048eb0863976e7ace0868cbd
Jayson Gamilla <gamillajayson.work@gmail.com>
2024-10-04 01:39:50 +0800

    Added view Jas profile API

--- app/Http/Controllers/Api/JasController.php
+++ app/Http/Controllers/Api/JasController.php
@@ -13,7 +13,8 @@ use File, DB;
 class JasController extends Controller
 {
 
-    public function getActivities(){
+    public function getActivities()
+    {
         $activities = JasActivity::orderBy('sort', 'asc')->get();
         return response()->json($activities);
     }
@@ -91,7 +92,8 @@ class JasController extends Controller
         }
     }
 
-    public function deleteMonitoringData(Int $id) {
+    public function deleteMonitoringData(int $id)
+    {
         try {
             $JasProfile = JasMonitoringData::find($id);
             if ($JasProfile === null) {
@@ -99,8 +101,8 @@ class JasController extends Controller
             }
             $JasProfile->delete();
             return response()->json('success');
-        } catch(Exception $e) {
-            return response()->json('error',500);
+        } catch (Exception $e) {
+            return response()->json('error', 500);
         }
     }
     public function getMonitoringData(?int $id = 0)
@@ -161,8 +163,9 @@ class JasController extends Controller
         }
     }
 
-        
-    public function deleteMonitoring(Int $id) {
+
+    public function deleteMonitoring(int $id)
+    {
         try {
             $JasProfile = JasMonitoring::find($id);
             if ($JasProfile === null) {
@@ -170,8 +173,8 @@ class JasController extends Controller
             }
             $JasProfile->delete();
             return response()->json('success');
-        } catch(Exception $e) {
-            return response()->json('error',500);
+        } catch (Exception $e) {
+            return response()->json('error', 500);
         }
     }
     public function getMonitoringgetMonitoringByProfile(?int $id = 0)
@@ -206,9 +209,9 @@ class JasController extends Controller
     }
     public function getMonitoring(int $id = 0)
     {
-        if($id>0){
+        if ($id > 0) {
             $jas = JasMonitoring::where('monitoring_id', $id)->get();
-        }else{
+        } else {
             $jas = JasMonitoring::all();
         }
 
@@ -223,13 +226,14 @@ class JasController extends Controller
 
         return response()->json($data);
     }
-    public function upsert(Request $request, ?Int $id = 0) {
+    public function upsert(Request $request, ?int $id = 0)
+    {
 
         DB::beginTransaction();
 
         try {
             $JasProfile = $id == 0 ? new JasProfile : JasProfile::find($id);
-        
+
             if ($request->hasFile('image')) {
                 $file = $request->file('image');
                 $name = $file->getClientOriginalName();
@@ -237,10 +241,10 @@ class JasController extends Controller
                 $file->move($path, $name);
                 $JasProfile->image = $path . '/' . $name;
             }
-        
+
             $JasProfile->fill($request->except('image'));
             $JasProfile->save();
-        
+
             DB::commit();
             return response()->json($JasProfile);
         } catch (Exception $e) {
@@ -249,8 +253,9 @@ class JasController extends Controller
         }
 
     }
-        
-    public function delete(Int $id) {
+
+    public function delete(int $id)
+    {
         try {
             $JasProfile = JasProfile::find($id);
             if ($JasProfile === null) {
@@ -258,51 +263,86 @@ class JasController extends Controller
             }
             $JasProfile->delete();
             return response()->json('success');
-        } catch(Exception $e) {
-            return response()->json('error',500);
+        } catch (Exception $e) {
+            return response()->json('error', 500);
         }
     }
 
-    public function getByTps(?Int $id = 0) {
+    public function getByTps(?int $id = 0)
+    {
         if ($id > 0) {
             $jas = JasProfile::where('technician', $id)->get();
         } else {
             return response()->json(['error' => 'No data found'], 404);
         }
-    
+
         $data = array();
         foreach ($jas as $j) {
             $jArray = $j->toArray();
             $jArray['image'] = asset($j->image);
             $data[] = $jArray;
         }
-    
+
         if ($jas->count() == 0) {
             return response()->json(['error' => 'No data found'], 404);
         }
-    
+
         return response()->json($data);
     }
-    public function get(?Int $id = 0) {
+    public function get(?int $id = 0)
+    {
         if ($id > 0) {
             $jas = JasProfile::where('id', $id)->get();
         } else {
             $jas = JasProfile::all();
         }
-    
+
         $data = array();
         foreach ($jas as $j) {
             $jArray = $j->toArray();
             $jArray['image'] = asset($j->image);
             $data[] = $jArray;
         }
-    
+
         if ($jas->count() == 0) {
             return response()->json(['error' => 'No data found'], 404);
         }
-    
+
         return response()->json($data);
     }
-    
+
+    public function getJasProfileData($id)
+    {
+        // Use the ID directly without decryption
+        $profile = JasProfile::with('technician', 'farmer')->find($id);
+
+        if (!$profile) {
+            return response()->json(['success' => false, 'message' => 'Profile not found'], 404);
+        }
+
+        // Fetch related monitoring and monitoring data
+        $monitoring = $profile->monitoring;
+        $monitoring_data = JasMonitoringData::where('jas_profile_id', $profile->id)
+            ->with('activity')
+            ->get();
+
+        // Fetch the activities for the PDF
+        $first_activity = JasActivity::where('pdf_table_no', 1)->get();
+        $second_activity = JasActivity::where('pdf_table_no', 2)->get();
+
+        // Return the profile data along with monitoring and activities
+        return response()->json([
+            'success' => true,
+            'data' => [
+                'profile' => $profile,
+                'monitoring' => $monitoring,
+                'monitoring_data' => $monitoring_data,
+                'first_activity' => $first_activity,
+                'second_activity' => $second_activity,
+            ]
+        ]);
+    }
+
+
 
 }
