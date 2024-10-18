1de984a51c32348faa921fd81d3bce151f5393e9
Jayson G <jayson.gamilla@recruitonline.com.au>
2024-10-18 14:58:08 +0800

    improve login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -95,20 +95,20 @@
                                             </td>
                                             <td>{{ Str::limit($webinar->title, 20, '...') }}</td>
                                             <td>{{ $isNotStarted ? date('M d, Y H:i', $startDate) : '-' }}</td>
-                                            <td>{{ $webinar->type == 0 ? 'Video' : 'Photo' }}</td>
+                                            <td>Webinar</td>
                                             <td class="text-center">
                                                 @if ($webinar->type == 0)
                                                     <!-- Facebook Video -->
                                                     <div id="fb-root"></div>
                                                     <script async defer crossorigin="anonymous"
                                                         src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v10.0"></script>
-                                                    <div class="fb-video" data-href="{{ $webinar->link }}" data-width="120"
+                                                    <div class="fb-video" data-href="{{ $webinar->link }}" data-width="200"
                                                         data-show-text="false"></div>
                                                 @elseif($webinar->type == 1)
-                                                    <!-- Small Image with Link -->
+                                                    <!-- Larger Image with Link -->
                                                     <a href="{{ $webinar->link }}" target="_blank">
                                                         <img src="{{ $imageUrl }}" alt="Webinar Photo"
-                                                            style="width: 100px; height: 50px; object-fit:cover;">
+                                                            style="width: 150px; height: auto; object-fit:cover;">
                                                     </a>
                                                 @endif
                                             </td>
