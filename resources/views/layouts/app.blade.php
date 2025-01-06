cdf6395b1ef598d3f593ef52478c837ef0d6cd38
Jayson G <jayson.gamilla@recruitonline.com.au>
2025-01-06 14:06:33 +0800

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,7 @@
     <title>@yield('title') </title>
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-    <!--     <link rel="icon" type="img/png" sizes="32x32" href="{{ asset('images/favicon-32x32.png') }}">
- -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('images/favicon.png') }}">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
         integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog=="
