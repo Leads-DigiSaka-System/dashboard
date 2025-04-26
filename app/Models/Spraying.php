35ce0477ef3c025b830fd62c785198fcf16bc603
Wendell <blanco.wendell15@gmail.com>
2025-04-26 11:23:39 +0800

    asd

--- app/Models/Spraying.php
+++ app/Models/Spraying.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class planner extends Model
+class Spraying extends Model
 {
     use HasFactory;
     protected $guarded = [];
